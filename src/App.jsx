88bdd6d09d6fc66f3109a8f9ac45e0510a28e290
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-04-13 12:13:46 +0530

    added locomotive and story responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
 import Home from "./Components/Home/Home"
 import Footer from "./Components/Footer/Footer"
 import Story from "./Components/Story/Story"
 
+import "locomotive-scroll/dist/locomotive-scroll.css"
+
 const App = () => {
+	const location = useLocation()
+
+	// Check if the current location is the Story page
+	const isStoryPage = location.pathname === "/story"
+
 	return (
 		<div>
-			<Navbar />
+			{!isStoryPage && <Navbar />}
 			<Routes>
 				<Route
 					index
@@ -20,7 +26,7 @@ const App = () => {
 					element={<Story />}
 				/>
 			</Routes>
-			<Footer />
+			{!isStoryPage && <Footer />}
 		</div>
 	)
 }
